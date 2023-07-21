73c8d812bb230fa3c7853d11b00db9a9cb43d7bb
Kunal <kunalmemane9150@gmail.com>
2023-07-21 19:58:02 +0530

    Added Div class

---
+++ Div.java
@@ -0,0 +1,9 @@
+public class Div {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int div = a / b;
+
+        System.out.println("Division of " + a + " and " + b + " is: " + div);
+    }
+}
