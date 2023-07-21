0dbdec63b80c509ab48746c716e831ac8d41147e
Kunal <kunalmemane9150@gmail.com>
2023-07-21 19:07:45 +0530

    Added Multiply feature

---
+++ Mul.java
@@ -0,0 +1,7 @@
+public class Mul {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int mul = a * b;
+        System.out.println("Multiplication of " + a + " and " + b + " is" + mul);
+    }
+}
