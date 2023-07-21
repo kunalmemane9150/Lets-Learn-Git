1a7f722c01100db5e0e36bea1dcb16c32bbd90a2
Kunal <kunalmemane9150@gmail.com>
2023-07-21 18:41:06 +0530

    Initial commit

---
+++ First.java
@@ -0,0 +1,9 @@
+class First {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum of two " + a + " and " + b + " is: " + sum);
+    }
+}
